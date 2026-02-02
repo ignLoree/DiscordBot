c0b8e98e25480176d7218479754fc139212b6cb0
lqrxnz <96952980+ignLoree@users.noreply.github.com>
2026-02-03 00:38:20 +0100

    bug fix

--- Vinili & Caffè Dev Bot/Commands/Staff/ticket.js
+++ Vinili & Caffè Dev Bot/Commands/Staff/ticket.js
@@ -165,8 +165,8 @@ module.exports = {
                     });
                 const closeButton = new ActionRowBuilder()
                     .addComponents(
-                        new ButtonBuilder().setCustomId('accetta').setLabel('☑️ Accetta e chiudi').setStyle(ButtonStyle.Success),
-                        new ButtonBuilder().setCustomId('rifiuta').setLabel('<:vegax:1443934876440068179> Rifiuta e mantieni aperto').setStyle(ButtonStyle.Secondary)
+                        new ButtonBuilder().setCustomId('accetta').setEmoji('<:vegacheckmark:1443666279058772028>').setLabel('Accetta e chiudi').setStyle(ButtonStyle.Success),
+                        new ButtonBuilder().setCustomId('rifiuta').setEmoji('<:vegax:1443934876440068179>').setLabel('Rifiuta e mantieni aperto').setStyle(ButtonStyle.Secondary)
                     );
                 return await interaction.editReply({
                     content: `<@${ticketDoc.userId}>`,
