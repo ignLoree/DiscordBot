6314602b20b2c9e85df56dec072260f0ca06223a
lqrxnz <96952980+ignLoree@users.noreply.github.com>
2026-02-05 15:51:12 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -15,7 +15,7 @@ const bots = [
         key: "official",
         label: "Ufficiale",
         start: "./Vinili & Caffè Bot Ufficiale/shard.js",
-        startupDelayMs: 8500
+        startupDelayMs: 6500
     }
 ];
 
