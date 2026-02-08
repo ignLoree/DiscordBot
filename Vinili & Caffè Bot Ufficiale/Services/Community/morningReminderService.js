d84a7aa55fe0b0e0987dc12c6f451af733b712f2
lqrxnz <96952980+ignLoree@users.noreply.github.com>
2026-02-08 19:15:19 +0100

    Update morningReminderService.js

--- Vinili & Caffè Bot Ufficiale/Services/Community/morningReminderService.js
+++ Vinili & Caffè Bot Ufficiale/Services/Community/morningReminderService.js
@@ -23,7 +23,7 @@ function decodeHtmlEntities(input) {
     .replace(/&amp;/g, '&')
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
-    .replace(/&eacute;/g, 'è)
+    .replace(/&eacute;/g, 'è')
     .replace(/&uuml;/g, 'u')
     .replace(/&rsquo;/g, "'")
     .replace(/&ldquo;|&rdquo;/g, '"');
